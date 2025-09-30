e64279ab7c5dec3bf1ac8d0b95c9e02126ca7be1
here-Mission <sumit513611@gmail.com>
2025-09-30 23:26:06 +0530

    new project push

---
+++ shopping-cart/src/Components/ProductList.jsx
@@ -0,0 +1,14 @@
+import ProductCard from "./ProductCard";
+const ProductList = ({products,setProducts}) => {
+    return ( 
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {products.map((product)=>(
+            <ProductCard product={product} key={product.id}/>
+          
+
+        ))}
+      </div>
+     );
+} 
+ 
+export default ProductList;
